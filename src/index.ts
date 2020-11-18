3bfeb120b48c8415c2c238edf51e7b6a5e396ae1
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-17 21:57:56 -0500

    Node ts up and running

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+import * as express from 'express';
+const app = express();
+
+app.listen(9001, () => {
+    console.log("App is running on PORT:: 9001")
+})
+
+app.get('/', (request, response) => {
+    response.send('HMW - Backend');
+});
