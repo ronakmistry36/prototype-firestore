8175bba292dc94d1245cf2e4332bcdcc8b1ded7b
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-17 22:16:31 -0500

    Firestore connected

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import * as express from 'express';
+import * as firebaseAdmin from 'firebase-admin';
+
 const app = express();
 
+const serviceAccount = require('../firestore-key.json');
+
+
 app.listen(9001, () => {
+    firebaseAdmin.initializeApp({
+        credential: firebaseAdmin.credential.cert(serviceAccount)
+    });
+
+    const db = firebaseAdmin.firestore();
+
+
     console.log("App is running on PORT:: 9001")
 })
 
