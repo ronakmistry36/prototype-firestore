217fb6b852026c16bff01b53ecc124eae2309410
Ronak Mistry <ronakmistry36@gmail.com>
2020-11-17 22:39:57 -0500

    Basic get request is working

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
 import * as express from 'express';
 import * as firebaseAdmin from 'firebase-admin';
 
+
 const app = express();
 
 const serviceAccount = require('../firestore-key.json');
-
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount)
+});
+const firestore = firebaseAdmin.firestore();
 
 app.listen(9001, () => {
-    firebaseAdmin.initializeApp({
-        credential: firebaseAdmin.credential.cert(serviceAccount)
-    });
-
-    const db = firebaseAdmin.firestore();
-
-
     console.log("App is running on PORT:: 9001")
 })
 
-app.get('/', (request, response) => {
-    response.send('HMW - Backend');
+app.get('/', async (request, response) => {
+    const users = await firestore.collection('users').get();
+    let payload = [];
+    users.forEach((doc) => {
+        payload.push(doc.data())
+    })
+    response.send(payload);
 });
+
+
+
+/*
+* User crud
+*
+*
+*
+* */
